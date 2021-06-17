5119a7b61e92568dc52078f9059567def602f310
robertfkillion@gmail.com <robertfkillion@gmail.com>
2021-06-17 15:48:54 -0400

    Continued building code. Still unfinished.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 This project is designed to help a user to dynamically create categories with percent values of a whole. This might be used by a teacher to establish grading categories, or a gamer to calculate percent chance of an encounter among various options.
+
+The project is currently unfinished. Right now, each new field is created with the appropriate new percentage, but I need to create the functions that will change each previous category's percentage value and the field where it is show on the html page. I also need to add these functions to the event listeners so that the percentages can change dynamically.
+
+It should also create field validation such that the user can not raise the percentage to an amount that would push the total above 100.
--- code.js
+++ code.js
@@ -1,25 +1,73 @@
-const category = {hold: "off",title: "category",percentage: 0};
-let changeableFillPercent;
-let defaultStart = Object.create(category);
-defaultStart.hold = "off";
-defaultStart.title = "Category";
-defaultStart.percentage = 100;
-let allCategoriees = [defaultStart];
-addListeners(document.getElementById("categoryList").firstChild.nextSibling,0);
+const category = {hold: false,title: "category",percentage: 0};
+let allCategories = [];
+newCategory(0);
+addListeners(document.getElementById("category0"));
 
-function changeDetail(reference,property,value) {
-    allCategoriees[reference][property] = value;
-    console.log(`Changed ${allCategoriees[reference].title} ${property} to ${value}.`)
+function newCategory(number) {
+    let defaultStart = Object.create(category);
+    defaultStart.hold = false;
+    defaultStart.title = `Category${number}`;
+    defaultStart.percentage = getNewPercentage();
+    allCategories = [...allCategories,defaultStart];
 }
 
-function addListeners(object,reference) {
-    object.firstChild.addEventListener("input",changeDetail(reference,"hold",this.value));
-    object.firstChild.nextSibling.addEventListener("input",changeDetail(reference,"title",this.value));
-    object.firstChild.nextSibling.nextSibling.addEventListener("input",changeDetail(reference,"percentage",this.value));
+function getNewPercentage() {
+    //find out what the total percent is as well, because if it's less than 100, new elements should just show the difference.
+    let changeableFillPercent = allCategories.length===0 ? 100 : 0;
+    let numberOfHeldElements=0;
+    for (const element of allCategories) {
+        if (!element.hold) {
+            changeableFillPercent+=element.percentage;
+        } else {
+            numberOfHeldElements++;
+        }
+    }
+    console.log(`Changeable fill percent is ${changeableFillPercent}`);
+    console.log(`Number of held elements is ${numberOfHeldElements}`);
+    let percentAvailable = changeableFillPercent/(allCategories.length+1-numberOfHeldElements);
+    return percentAvailable;
+}
+
+function changeHold(event) {
+    let newVal = event.target.checked;
+    let target = event.target.parentElement.id.slice(-1);
+    allCategories[target].hold = newVal;
+}
+
+function changeTitle(event) {
+    let newVal = event.target.value;
+    let target = event.target.parentElement.id.slice(-1);
+    allCategories[target].title = newVal;
+}
+
+function changePercentage(event) {
+    let newVal = parseInt(event.target.value);
+    let target = event.target.parentElement.id.slice(-1);
+    allCategories[target].percentage = newVal;
+}
+
+function addListeners(object) {
+    object.firstChild.addEventListener("input",changeHold);
+    object.firstChild.nextSibling.addEventListener("input",changeTitle);
+    object.firstChild.nextSibling.nextSibling.addEventListener("input",changePercentage);
 }
 
 function addNew() {
-    x = document.getElementById("categoryList").firstChild.nextSibling.firstChild.nextSibling.value;
-    //x = document.getElementById("categoryList").firstChild.nextSibling;
-    alert(x);
+    let listElement = document.createElement("li");
+    let orderNum = allCategories.length;
+    newCategory(orderNum);
+    listElement.id = `category${orderNum}`;
+    document.getElementById("categoryList").appendChild(listElement);
+    let box = document.createElement("input");
+    box.type = "checkbox";
+    listElement.appendChild(box);
+    let title = document.createElement("input");
+    title.type = "text";
+    title.value = "Category"
+    listElement.appendChild(title);
+    let percent = document.createElement("input");
+    percent.type = "number";
+    percent.value = allCategories[orderNum].percentage;
+    listElement.appendChild(percent);
+    addListeners(listElement);
 }
